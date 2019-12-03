68313665b9024e510ac70cf673cd35987da81364
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-12-03 13:18:11 -0700

    Got npm start working, added dotenv and start script, adding authorization functionality

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
 
-const movies = require('./movieData')
+const movies = require("./movieData");
 
 const app = express();
 
-app.get('/movie', (req,res) => {
-    
+app.use(morgan("dev"));
+app.use(cors());
+app.use(helmet());
 
-    res.json(movies)
-})
+function requireAuth(req, res, next) {
+  const authVal = req.get("Authorization") || "";
 
-app.listen(8000, () => console.log('listening to server'))
+  if (!authVal.startsWith("Bearer ")) {
+    return res
+      .status(400)
+      .json({ message: "Required Bearer Authorization header not present" });
+  }
+
+  const token = authVal.split(" ")[1];
+  if (token !== process.env.API_TOKEN) {
+    return res.status(401).json({ message: "Invalid auth credentials" });
+  }
+
+  next();
+}
+
+app.get("/movie", (req, res) => {
+  res.json(movies);
+});
+
+app.listen(8000, () => console.log("listening to server"));
