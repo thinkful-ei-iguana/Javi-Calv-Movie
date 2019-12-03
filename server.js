9e22bac87a57cd3d73dd302e48a245764ba94614
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-12-03 14:12:46 -0700

    genre working, stuck on country and avg_vote

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 function requireAuth(req, res, next) {
   const authVal = req.get("Authorization") || "";
-    console.log(authVal)
+  console.log(authVal);
   if (!authVal.startsWith("Bearer ")) {
     return res
       .status(400)
@@ -22,7 +22,7 @@ function requireAuth(req, res, next) {
   }
 
   const token = authVal.split(" ")[1];
-  console.log(token,process.env.API_TOKEN)
+  console.log(token, process.env.API_TOKEN);
   if (token !== process.env.API_TOKEN) {
     return res.status(401).json({ message: "Invalid auth credentials" });
   }
@@ -31,19 +31,29 @@ function requireAuth(req, res, next) {
 }
 
 app.get("/movie", requireAuth, (req, res) => {
-    let response = movies
-    const genre = req.query.genre;
-    const country = req.query.country;
-    const avg_vote = req.query.avg_vote;
+  let response = movies;
+  const genre = req.query.genre;
+  const country = req.query.country;
+  const avg_vote = req.query.avg_vote;
 
-
-    if(genre){
-        console.log(genre)
-        response = response.filter(movie => movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()))
-    }
-
-
-
+  if (genre) {
+    console.log(genre);
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+  if (country) {
+    console.log(country);
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+  if (avg_vote) {
+    console.log(avg_vote);
+    response = response.filter(
+      movie => Number(req.query.avg_vote) >= Number(req.query.avg_vote)
+    );
+  }
   res.json(response);
 });
 
